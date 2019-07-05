621547c5d346d6235d1c6bd0585d3446e147ec43
FREIBURGER <bastien.freiburger@cgi.com>
2019-07-05 09:33:38 +0200

    [MESI][DEV] Initialisation du projet : springboot+liquibase

---
+++ src/main/java/com/ipicascadeteam/mesi/MesiApplication.java
@@ -0,0 +1,13 @@
+package com.ipicascadeteam.mesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesiApplication.class, args);
+	}
+
+}
